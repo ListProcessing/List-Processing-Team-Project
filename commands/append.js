9ee6c2cf8abad9eb971692101196e44961aa8387
Ivan Pashkulev <ivan.pashkulev@gmail.com>
2017-07-19 18:59:36 +0300

    Updated append and prepend commands by adding validations for empty string

--- commands/append.js
+++ commands/append.js
@@ -1,6 +1,6 @@
 define([], function() {
     function append(arr, commandTokens) {
-        if (commandTokens[0].trim() === '') {
+        if (commandTokens.length === 0 || commandTokens.length > 1 || commandTokens[0].trim() === '') {
             throw new Error("Error: invalid command parameters");
         }
         arr.push(commandTokens[0]);
--- commands/prepend.js
+++ commands/prepend.js
@@ -1,6 +1,6 @@
 define([], function() {
     function prepend(arr, commandTokens) {
-        if (commandTokens[0].trim() === '') {
+        if (commandTokens.length === 0 || commandTokens.length > 1 || commandTokens[0].trim() === '') {
             throw new Error("Error: invalid command parameters");
         }
         arr.unshift(commandTokens[0]);
